c70e33c47aa7234633f72463261ca55fa36a2e05
luccerboi <luc.walterbos@hotmail.nl>
2026-07-16 19:36:57 +0200

    added calculation batching to solve OOM errors

--- E_above_hull/run_ehull_calcs.py
+++ E_above_hull/run_ehull_calcs.py
@@ -4,27 +4,34 @@ from pymatgen.core import Structure
 import os
 import json
 import sys
-
+from math import ceil
 
 
 mlip = sys.argv[1]
-
+batchnum = int(sys.argv[2])
+batchsize = 500
 
 to_compute = ["MatterSim","MatPES-r2SCAN", "MACE-MP-0b3", "MACE-MPA-0", "Nequip"]  # Nequip -> Nequip-OAM-XL
 assert mlip in to_compute, f"MLIP argument must be in {to_compute}."
 
 ## Read chemical systems to simulate and convert structure dicts to structure objects
-subsys_df = pd.read_csv('hdp_mp_subsys_df.csv',index_col=0)
+subsys_df = pd.read_csv('hdp_mp_sybsysandhdps.csv',index_col=0)
 subsys_df['structure'] = subsys_df.apply(lambda row: Structure.from_dict(eval(row['structure_dict'])),axis=1)
 
-hdp_df = pd.read_csv('hdp_hdps_mlipenergies.csv', index_col=0)
-hdp_df['structure'] = hdp_df.apply(lambda row: Structure.from_dict(eval(row['structure_dict'])))
 
 data_dir = "MLIP_data"
 os.makedirs(data_dir, exist_ok=True)
 
+n_batches = ceil(len(subsys_df)/batchsize)
+try:
+    batch_df = subsys_df.iloc[batchnum * batchsize : (batchnum+1)*batchsize]
+except IndexError:
+    batch_df = subsys_df.iloc[batchnum*batchsize:]
+else:
+    print("WEIRD ERROR IN SPLITTING DATA")
 
-print(f"Starting {mlip} for subsystems")
+
+print(f"Starting {mlip} for batchnumber {batchnum}")
 
 try:
     mpid_energy_dict = mlip_relax_and_get_energies(structure_dict= subsys_df['structure'], 
@@ -33,24 +40,11 @@ except Exception as e:
     print(f"Encountered {str(e)} in {mlip}  exiting now!")
     exit(1)
 
-with open(os.path.join(data_dir, f"mpid_energy_dict_{mlip}.json"), "w") as f:
+with open(os.path.join(data_dir, f"mpid_energy_dict_{mlip}_batch_{batchnum}.json"), "w") as f:
     json.dump(mpid_energy_dict, f)
 
-print(f"Finished subsystems with {mlip}")
 
-try:
-    hdp_energy_dict = mlip_relax_and_get_energies(
-        structure_dict=hdp_df['structure'],
-        force_field_name=mlip
-    )
-except Exception as e:
-    print(f"Encountered {str(e)} in {mlip}  exiting now!")
-    exit(1)
-
-with open(os.path.join(data_dir,f"hdp_energy_dict_{mlip}.json"),"w") as f:
-    json.dump(hdp_energy_dict,f)
-
-print(f"Finished {mlip} flow no.")
+print(f"Finished {mlip} flow no. {batchnum}")
 
 
 
