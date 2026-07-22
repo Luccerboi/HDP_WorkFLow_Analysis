e5ddd2313e6a1eaccaded4df271386e952e6879f
luccerboi <luc.walterbos@hotmail.nl>
2026-07-22 17:37:03 +0200

    -Added more parallelization to workflow running -Added functionality to make phase diagrams and extract e above hull energies -MLIP energies for all subsystems ran with the first iteration of MLIPs, more following soon

--- E_above_hull/run_ehull_calcs.py
+++ E_above_hull/run_ehull_calcs.py
@@ -3,23 +3,72 @@ import pandas as pd
 from pymatgen.core import Structure
 import os
 import json
+import torch
 import sys
 from math import ceil
 
 
 mlip = sys.argv[1]
-batchnum = int(sys.argv[2])
-batchsize = 500
+batchsize = int(sys.argv[2])
+batchnum = int(sys.argv[3])
 
-to_compute = ["MatterSim","MatPES-r2SCAN", "MACE-MP-0b3", "MACE-MPA-0", "Nequip"]  # Nequip -> Nequip-OAM-XL
-assert mlip in to_compute, f"MLIP argument must be in {to_compute}."
+to_compute = ["MACE-Gabor","SevenNet","MatterSim","MatPES-r2SCAN", "MACE-MP-0b3", "MACE-MPA-0", "Nequip"]  # Nequip -> Nequip-OAM-XL
+#assert mlip in to_compute, f"MLIP argument must be in {to_compute}."
 
 ## Read chemical systems to simulate and convert structure dicts to structure objects
-subsys_df = pd.read_csv('hdp_mp_sybsysandhdps.csv',index_col=0)
-subsys_df['structure'] = subsys_df.apply(lambda row: Structure.from_dict(eval(row['structure_dict'])),axis=1)
+subsys_df = pd.read_csv('hdp_mp_subsysandhdps.csv',index_col=0)
+
+relax_kwargs = {
+    # "steps" : 1000,
+    "fmax" : 0.05,
+    "maxstep": 0.03,
+}
+optimizer_kwargs = {
+
+    }
+
+if mlip == "Nequip":
+    relax_kwargs["maxstep"] = 0.001
+    mlip_name = "Nequip"
+    mlip_kwargs = {
+            "compile_path": "/home/lwalterb/NequIP_OAM_XL/mir-group__NequIP-OAM-XL__0.1.nequip.pt2",
+            "device":"cuda",
+            "default_dtype":"float64",
+        }
+elif mlip.startswith('MatPES'):
+    mlip_name = mlip
+    mlip_kwargs = {
+            'device':'cuda',
+            'default_dtype':'float64'
+                   }
+
+    relax_kwargs['maxstep'] = 0.02 #MatPes has some issues so smaller increments
+    #mlip_kwargs = {"default_dtype":"float64"}
+
+elif mlip == "MACE-Gabor":
+    mlip_name = "MACE"
+    mlip_kwargs = {
+        'model' : "/home/lwalterb/hdp_ehull/MACE_ALT_GABOR/MACE-MP-0b3_GaborShift.model",
+        'device' : 'cuda',
+        'default_dtype' : 'float64',
+    }
+elif mlip == "MACE-Wang":
+    mlip_name = "MACE"
+    mlip_kwargs = {
+        'model' : "/home/lwalterb/hdp_ehull/MACE_ALT_GABOR/MACE-MP-0b3_WangShift.model",
+        'device' : 'cuda',
+        'default_dtype' : 'float64',
+    }
+
+else:
+    mlip_name = mlip
+    mlip_kwargs = {
+            "default_dtype":"float64",
+            'device':'cuda',
+                   }
 
 
-data_dir = "MLIP_data"
+data_dir = "MLIP_race"
 os.makedirs(data_dir, exist_ok=True)
 
 n_batches = ceil(len(subsys_df)/batchsize)
@@ -27,31 +76,22 @@ try:
     batch_df = subsys_df.iloc[batchnum * batchsize : (batchnum+1)*batchsize]
 except IndexError:
     batch_df = subsys_df.iloc[batchnum*batchsize:]
-else:
-    print("WEIRD ERROR IN SPLITTING DATA")
 
+batch_df['structure'] = batch_df.apply(lambda row: Structure.from_dict(eval(row['structure_dict'])),axis=1)
 
 print(f"Starting {mlip} for batchnumber {batchnum}")
 
-try:
-    mpid_energy_dict = mlip_relax_and_get_energies(structure_dict= subsys_df['structure'], 
-                                                    force_field_name=mlip )
-except Exception as e:
-    print(f"Encountered {str(e)} in {mlip}  exiting now!")
-    exit(1)
+
+mpid_energy_dict = mlip_relax_and_get_energies(structure_dict= batch_df['structure'], 
+                                                    force_field_name=mlip_name,
+                                                    calculator_kwargs=mlip_kwargs,
+                                                    relax_kwargs= relax_kwargs,
+                                                    optimizer_kwargs = optimizer_kwargs,
+                                               )
 
 with open(os.path.join(data_dir, f"mpid_energy_dict_{mlip}_batch_{batchnum}.json"), "w") as f:
     json.dump(mpid_energy_dict, f)
 
 
 print(f"Finished {mlip} flow no. {batchnum}")
-
-
-
-
-# # "float64" for geom. optimization
-# calculator_kwargs = {"default_dtype": "float64", 
-#                     #  "compile_path": "/home/htc/kueltzen/HK_hydride_project/nequip-model/NequIP-OAM-XL.nequip.pt2", 
-#                      "device": "cpu"} \
-#     if mlip == "Nequip" else {"default_dtype": "float64"}
 
